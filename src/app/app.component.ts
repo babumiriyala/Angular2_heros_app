7fb0498e1e83be18e41f3cb766f6d6af189f8035
Babu Miriyala <BabuMiriyala@Babu-Miryala.local>
2017-08-29 11:53:17 +0530

    You created a reusable component. You learned how to make a component accept input. You learned to declare the required application directives in an NgModule. You listed the directives in the @NgModule decorator's declarations array. You learned to bind a parent component to a child component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +10,15 @@ import { Component } from '@angular/core';
   template: `
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
-<ul class="heroes">
-<li *ngFor="let hero of heroes"  [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
-<span class="badge">{{hero.id}}</span> {{hero.name}}
-</li>
-</ul>
-<div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}} details!</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-  </div>
-</div>
-  `,
+  <ul class="heroes">
+    <li *ngFor="let hero of heroes"
+      [class.selected]="hero === selectedHero"
+      (click)="onSelect(hero)">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
+  </ul>
+  <hero-detail [hero]="selectedHero"></hero-detail>
+`,
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
@@ -88,10 +84,7 @@ export class AppComponent {
 
 
 
-export class Hero {
-  id: number;
-  name: string;
-}
+
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
